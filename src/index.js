f3f7434d30e4a85940ebfa6cc85ee80122d5ce95
silvestrevivo <silvestrevivomillan@gmail.com>
2018-02-21 14:20:01 +0100

    setting up working

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../assets/sass/style.scss'
+import App from './components/app'
+
+ReactDOM.render(<App />, document.getElementById('root'))
