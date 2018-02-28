8a0c6f678890bf1564a2414a6353fe9c7943f1a9
silvestrevivo <silvestrevivomillan@gmail.com>
2018-02-28 21:34:19 +0100

    update package.json and layout

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const App = () => (
-  <img
-    src="https://cdn-images-1.medium.com/max/1600/1*ypTuZbQNEV1oGkAfn85AUA.png"
-    alt="logo-react" />
+  <div className="container">
+    <img
+      src="https://cdn-images-1.medium.com/max/1600/1*ypTuZbQNEV1oGkAfn85AUA.png"
+      alt="logo-react" />
+    <h1>React Starter</h1>
+  </div>
+
 )
 
 export default App
