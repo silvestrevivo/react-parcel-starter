ec9eb5cec4425e70c81a527e8a705e816d8085a0
silvestrevivo <silvestrevivomillan@gmail.com>
2018-08-22 22:21:42 +0200

    updating eslint with prettier from modules

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { hot } from 'react-hot-loader'
+import React from 'react';
+import { hot } from 'react-hot-loader';
 
 const App = () => (
   <div className="container">
     <img
       src="https://cdn-images-1.medium.com/max/1600/1*ypTuZbQNEV1oGkAfn85AUA.png"
-      alt="logo-react" />
+      alt="logo-react"
+    />
     <h1>React Starter</h1>
   </div>
-)
+);
 
-export default hot(module)(App)
+export default hot(module)(App);
